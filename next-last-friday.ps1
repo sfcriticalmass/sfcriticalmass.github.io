3138a3d5bfa5cc4aacce416f5066e33a47440a32
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-04-23 10:06:40 -0700

    Update next-last-friday.ps1 comments

--- next-last-friday.ps1
+++ next-last-friday.ps1
@@ -2,8 +2,8 @@ param ([System.DateTime] $date = (Get-Date))
 
 # Next Friday.
 $date = $date.AddDays((12 - $date.DayOfWeek) % 7)
-# First day of next month for next Friday.
+# First day of month after that.
 $date = $date.AddDays(1 - $date.Day).AddMonths(1)
-# Last Friday before first day of next month for next Friday.
+# The closest Friday before that.
 $date = $date.AddDays(-((($date.DayOfWeek + 2) - 1) % 7 + 1))
 $date
